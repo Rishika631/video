7e20268b574458f45648eab3fa4796a71dec3206
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-02 16:24:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from transformers import pipeline
 from youtube_transcript_api import YouTubeTranscriptApi
-from PyPDF2 import PdfWriter
-import base64
 
 # Set Streamlit page configuration
 st.set_page_config(page_title="YouTube Video Summarizer and Q&A")
@@ -34,31 +32,10 @@ def summarize_transcript(transcript):
     
     return summary
 
-# Function to generate PDF with transcript summary
-def generate_pdf(summary):
-    pdf_path = "transcript_summary.pdf"
-    pdf_writer = PdfWriter()
-    pdf_writer.add_page()
-    pdf_writer.set_font("Arial", size=12)
-    
-    pdf_writer.cell(0, 10, txt="Summary:", ln=True)
-    pdf_writer.cell(0, 10, txt=summary, ln=True)
-    
-    with open(pdf_path, "wb") as f:
-        pdf_writer.write(f)
-    
-    return pdf_path
-
-# Function to perform question-answering on the PDF
-def perform_qa(pdf_path, question):
+# Function to perform question-answering
+def perform_qa(text, question):
     qa_model = pipeline("question-answering", model="bert-large-uncased-whole-word-masking-finetuned-squad")
-    
-    with open(pdf_path, "rb") as f:
-        pdf_bytes = f.read()
-    
-    context = pdf_bytes.decode("utf-8")
-    answer = qa_model(question=question, context=context)
-    
+    answer = qa_model(question=question, context=text)
     return answer["answer"]
 
 # Streamlit app
@@ -75,9 +52,6 @@ def main():
         # Summarize transcript
         summary = summarize_transcript(transcript)
         
-        # Generate PDF with transcript summary
-        pdf_path = generate_pdf(summary)
-        
         st.info("Transcript summary generated successfully!")
         
         # Display transcript summary
@@ -88,8 +62,8 @@ def main():
         user_question = st.text_input("Ask a question about the video:")
         
         if user_question:
-            # Perform question-answering on the PDF
-            answer = perform_qa(pdf_path, user_question)
+            # Perform question-answering on the summarized text
+            answer = perform_qa(summary, user_question)
             
             st.subheader("Question-Answering")
             st.write(answer)
