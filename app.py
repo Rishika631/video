7cf6f0673e36146b7dd2744fff59735c6e995c7a
Rishika631 <89201634+Rishika631@users.noreply.github.com>
2023-07-02 16:10:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,57 @@
-import streamlit as st
-import PyPDF2
-import moviepy.editor as mp
-from transformers import pipeline
+
+from transformers import pipeline, T5Tokenizer
+from youtube_transcript_api import YouTubeTranscriptApi
 from PyPDF2 import PdfWriter
+import nltk
+from nltk.tokenize import sent_tokenize
+import base64
 
 # Set Streamlit page configuration
-st.set_page_config(page_title="Video Transcript and Q&A")
+st.set_page_config(page_title="YouTube Video Transcript Summarizer and Q&A")
 
-# Function to extract transcript from video
-def extract_transcript(video_file):
-    video = mp.VideoFileClip(video_file)
-    video_audio = video.audio
-    temp_audio_file = "temp.wav"
-    video_audio.write_audiofile(temp_audio_file)
-
-    transcriber = pipeline("automatic-speech-recognition")
-    transcript = transcriber(temp_audio_file)[0]['alternatives'][0]['text']
+# Function to extract transcript from YouTube video
+def extract_transcript(youtube_video):
+    video_id = youtube_video.split("=")[-1]
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
     
-    return transcript
+    transcript_text = ""
+    for segment in transcript:
+        transcript_text += segment['text'] + " "
+    
+    return transcript_text
 
-# Function to generate PDF with transcript
-def generate_pdf(transcript):
-    pdf_path = "transcript.pdf"
+# Function to summarize transcript
+def summarize_transcript(transcript):
+    # Tokenize transcript into sentences
+    sentences = sent_tokenize(transcript)
+    
+    # Select a summary length based on the number of sentences
+    summary_length = max(int(len(sentences) * 0.3), 1)
+    
+    # Join sentences back into a single string for summarization
+    input_text = " ".join(sentences)
+    
+    # Use T5 model for summarization
+    summarization_model = pipeline("summarization", model="t5-base", tokenizer="t5-base")
+    
+    # Generate summary
+    summary = summarization_model(input_text, max_length=summary_length, min_length=summary_length)
+    summary_text = summary[0]['summary_text']
+    
+    return summary_text
+
+# Function to generate PDF with transcript and summary
+def generate_pdf(transcript, summary):
+    pdf_path = "transcript_summary.pdf"
     pdf_writer = PdfWriter()
     pdf_writer.add_page()
     pdf_writer.set_font("Arial", size=12)
+    
+    pdf_writer.cell(0, 10, txt="Transcript:", ln=True)
     pdf_writer.cell(0, 10, txt=transcript, ln=True)
+    
+    pdf_writer.cell(0, 10, txt="\nSummary:", ln=True)
+    pdf_writer.cell(0, 10, txt=summary, ln=True)
     
     with open(pdf_path, "wb") as f:
         pdf_writer.write(f)
@@ -34,8 +60,7 @@ def generate_pdf(transcript):
 
 # Function to perform question-answering on the PDF
 def perform_qa(pdf_path, question):
-    model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
-    qa_model = pipeline("question-answering", model=model_name)
+    qa_model = pipeline("question-answering", model="bert-large-uncased-whole-word-masking-finetuned-squad")
     
     with open(pdf_path, "rb") as f:
         pdf_bytes = f.read()
@@ -47,32 +72,28 @@ def perform_qa(pdf_path, question):
 
 # Streamlit app
 def main():
-    st.header("Video Transcript and Q&A")
+    st.header("YouTube Video Transcript Summarizer and Q&A")
     
-    # Upload video file
-    video_file = st.file_uploader("Upload a video file (.mp4)", type="mp4")
+    # Get YouTube video URL from user
+    youtube_video = st.text_input("Enter the YouTube video URL:")
     
-    if video_file is not None:
-        # Extract transcript from video
-        transcript = extract_transcript(video_file)
+    if youtube_video:
+        # Extract transcript from YouTube video
+        transcript = extract_transcript(youtube_video)
         
-        # Generate PDF with transcript
-        pdf_path = generate_pdf(transcript)
+        # Summarize transcript
+        summary = summarize_transcript(transcript)
         
-        st.info("Transcript generated successfully!")
+        # Generate PDF with transcript and summary
+        pdf_path = generate_pdf(transcript, summary)
         
-        # Display transcript and provide input for question
+        st.info("Transcript and Summary generated successfully!")
+        
+        # Display transcript and summary
         st.subheader("Transcript")
         st.text_area("Transcript", transcript, height=300)
         
-        question = st.text_input("Ask a question about the transcript")
+        st.subheader("Summary")
+        st.text(summary)
         
-        if question:
-            # Perform question-answering on the PDF
-            answer = perform_qa(pdf_path, question)
-            st.subheader("Question-Answering Result")
-            st.write(answer)
-
-# Run the Streamlit app
-if __name__ == "__main__":
-    main()
+        # Provide
